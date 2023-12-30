6e2cbbe146a82854083ff3e5d2c3b5e0e43bff46
kylealvares <47119553+kylealvares@users.noreply.github.com>
2023-12-30 16:15:26 +0530

    chore: project setup 🚀

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import styles from './Home.module.css';
+
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <div className={styles.centered}>
+                <h1>Goa</h1>
+            </div>
+        </div>
+    )
+}
